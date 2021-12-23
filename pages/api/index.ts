bea566b8f3e0650ac5ee776ea92ba5ed66a93dd1
Zakiego <belummikir@gmail.com>
2021-12-23 15:35:41 +0800

    fix: add index api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -3,12 +3,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
+  message: string;
 };
 
 export default function helloAPI(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ message: "Hi" });
 }
