76a6f0df34abab7f4e324d889ee2cdeb8ba273af
Zakiego <belummikir@gmail.com>
2021-12-23 16:27:50 +0800

    feat: add title in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-black">
-      <h1 className="m-auto text-2xl text-white">Hi</h1>
-    </div>
+    <>
+      <Head>
+        <title>Zakiego</title>
+      </Head>
+      <div className="flex min-h-screen bg-black">
+        <h1 className="m-auto text-2xl text-white">
+          <a href="https://twitter.com/prasastipagi">Hi</a>
+        </h1>
+      </div>
+    </>
   );
 }
