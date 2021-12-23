b06abf8299eaf068593d8147ff12529dd5667646
Zakiego <belummikir@gmail.com>
2021-12-23 16:32:44 +0800

    feat: change link in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       </Head>
       <div className="flex min-h-screen bg-black">
         <h1 className="m-auto text-2xl text-white">
-          <a href="https://twitter.com/prasastipagi">Hi</a>
+          <a href="https://github.com/zakiego/app.zakiego.my.id">Hi</a>
         </h1>
       </div>
     </>
