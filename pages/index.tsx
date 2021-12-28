c30188dc135d8553ffabd40a155e364112552245
Zakiego <belummikir@gmail.com>
2021-12-28 18:07:32 +0800

    feat: add github link on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,13 @@ export default function Home() {
       </Head>
       <div className="flex min-h-screen bg-black">
         <h1 className="m-auto text-2xl text-white">
-          <a href="https://github.com/zakiego/app.zakiego.my.id">Hi</a>
+          <div className="text-6xl">Hi</div>
+          <a
+            className="underline transition-all underline-offset-2 hover:opacity-75"
+            href="https://github.com/zakiego/app.zakiego.my.id"
+          >
+            Github
+          </a>
         </h1>
       </div>
     </>
